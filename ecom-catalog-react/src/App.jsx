b2b49e94afcff46e4a075ed40ba345d7fcde71aa
Davinder47 <davinder9730@gmail.com>
2025-11-02 01:09:46 +0530

    Updated config

--- ecom-catalog-react/src/App.jsx
+++ ecom-catalog-react/src/App.jsx
@@ -1,16 +1,32 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import ProductList from './ProductList';
 
 function App() {
+  const [products, setProducts] = useState([]);
+
+  //Below is Useeffect hook where we are making use of Javascript fetch 
+  //to do an API call to our Backend, and we are getting the response in
+  //json and then we are setting this json to the product state
+
+  useEffect(() => {
+    fetch('http://localhost:8080/api/products')
+      .then(response => response.json())
+      .then(data => setProducts(data));
+  }, []);
   return (
-    <div>
-      <button type="button" class="btn btn-outline-primary">Primary</button>
-      <button type="button" class="btn btn-outline-secondary">Secondary</button>
-      <button type="button" class="btn btn-outline-success">Success</button>
-      <button type="button" class="btn btn-outline-danger">Danger</button>
-      <button type="button" class="btn btn-outline-warning">Warning</button>
-      <button type="button" class="btn btn-outline-info">Info</button>
-      <button type="button" class="btn btn-outline-light">Light</button>
-      <button type="button" class="btn btn-outline-dark">Dark</button>
+    // {/* To display the data on the frontend */}
+    <div className='container'>
+      <div>
+        {products.length ? (
+          //Display Products
+          <ProductList products={products}/>
+        ) : (
+          <p>No Products Found</p>
+        )}
+      </div>
+  
+
     </div>
   )
 }
